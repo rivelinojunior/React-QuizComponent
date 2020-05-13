e632ce162e07b6fa12ebd199df09af8cde12aeda
Rivelino Junior <rv.fernandes.junior@gmail.com>
2020-05-12 22:45:26 -0400

    feat: create QuizQuestion Component

---
+++ src/QuizQuestion.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class QuizQuestion extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <main>
+        <section>
+          <p>{this.props.quizQuestion.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            <li>{this.props.quizQuestion.answer_options[0]}</li>
+          </ul>
+        </section>
+      </main>
+    )
+  }
+}
+
+export default QuizQuestion
