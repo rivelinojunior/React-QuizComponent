2732d47d4bae0725cf3ce2d8f3210991eef33918
Rivelino Junior <rv.fernandes.junior@gmail.com>
2020-05-13 00:38:02 -0400

    feat: create QuizEnd component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion'
+import QuizEnd from './QuizEnd'
 
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
+
   constructor(props) {
     super(props)
     this.state = { quiz_position: 1 }
   }
+
   render() {
-    return (
-      <div>
-        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
-      </div>
-    )
+    const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
+
+    if(isQuizEnd) {
+      return (
+        <div><QuizEnd/></div>
+      )
+    } else {
+      return (
+        <div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/></div>
+      )
+    }
   }
 }
 
